206582d5c551b9804c2b784b560f25cd41698be2
mahazoasy <mahazoasy.h@zurcher.edu.mg>
2026-04-20 16:44:01 +0300

    Modification

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+// import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,11 +17,13 @@ async function bootstrap() {
     .setDescription('API REST de gestion de bibliothèque — Cours 2026')
     .setVersion('1.0')
     .addBearerAuth()
-    .addServer('http://localhost:3000/api/v1', 'Développement')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/documentation', app, document);
 
   await app.listen(3000);
+  // cette chose est utiliser pour generer le openapi.json et je déjà le generer alors je le commenter 
+  // fs.writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
+  // console.log('Fichier openapi.json généré');
 }
 bootstrap();
