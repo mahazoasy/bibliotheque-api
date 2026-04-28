02fcb4dc09b94b26a1d495fadd4cc57bcd0aaf0d
mahazoasy <mahazoasy.h@zurcher.edu.mg>
2026-04-28 21:49:26 +0300

    Modification

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -9,7 +9,10 @@ export class StripeService {
 
   constructor(private config: ConfigService) {
     const secretKey = this.config.getOrThrow<string>('STRIPE_SECRET_KEY');
-    this.stripe = new Stripe(secretKey);
+    this.stripe = new Stripe(secretKey, {
+      timeout: 120000,      // 120 secondes (2 minutes)
+      apiVersion: '2025-03-31.basil',
+    });
   }
 
   async createPaymentIntent(userId: string, amount = 500, currency = 'eur'): Promise<any> {
